ba07767f1488e991d1ab4939c701154ca61ba8d7
John McLear <john@mclear.co.uk>
2020-11-22 15:19:02 +0000

    autofixes from Etherpad checkPlugins.js

--- static/js/desktop_notifications.js
+++ static/js/desktop_notifications.js
@@ -1,129 +1,129 @@
 var DesktopNotifications = {
   /* compatilibility layer */
-  notificationSupported: function() {
+  notificationSupported() {
     return !!window.Notification || !!window.webkitNotifications;
   },
-  notificationPermission: function() {
+  notificationPermission() {
     if (window.Notification) {
       return Notification.permission;
     } else if (window.webkitNotifications) {
-      return webkitNotifications.checkPermission() === 0 ? "granted" : "default";
+      return webkitNotifications.checkPermission() === 0 ? 'granted' : 'default';
     } else {
-      return "denied";
+      return 'denied';
     }
   },
-  requestNotificationPermission: function(cb) {
+  requestNotificationPermission(cb) {
     if (window.Notification) {
       return Notification.requestPermission().then(cb);
     } else if (window.webkitNotifications) {
-      return webkitNotifications.requestPermission(function() {
+      return webkitNotifications.requestPermission(() => {
         cb(DesktopNotifications.notificationPermission());
       });
     } // else just chill
   },
-  createNotification: function(authorName, text, lang) {
+  createNotification(authorName, text, lang) {
     if (window.Notification) {
-      new Notification(authorName, { body: text, lang: lang });
+      new Notification(authorName, {body: text, lang});
     } else if (window.webkitNotifications) {
-      window.webkitNotifications.createNotification("", authorName, text).show();
+      window.webkitNotifications.createNotification('', authorName, text).show();
     } else {
-      console.warn(authorName + " said: " + text);
+      console.warn(`${authorName} said: ${text}`);
     }
   },
 
-  allowNotificationsToolbarEntryEnabled: function(status) {
+  allowNotificationsToolbarEntryEnabled(status) {
     status = !!status;
-    var previousEntry = $("#allowNotificationsToolbarEntry");
-    var previousStatus = previousEntry.length > 0;
+    const previousEntry = $('#allowNotificationsToolbarEntry');
+    const previousStatus = previousEntry.length > 0;
     if (status === previousStatus) {
       return;
     }
     if (status) {
-      allowNotificationsToolbarEntry = $("<li>").attr({
-        "id": "allowNotificationsToolbarEntry",
-        "data-type": "button",
-        "lang": "en",
-      }).click(function() {
+      allowNotificationsToolbarEntry = $('<li>').attr({
+        'id': 'allowNotificationsToolbarEntry',
+        'data-type': 'button',
+        'lang': 'en',
+      }).click(() => {
         DesktopNotifications.enable();
-      }).append($("<a>").attr({
-        "title": "Allow or forbid notifications",
-        "aria-label": "Allow or forbid notifications",
-      }).append($("<button>").attr({
-        "id": "notification-permission-button",
-        "class": "buttonicon buttonicon-chat",
-        "title": "Allow or forbid notifications",
-        "aria-label": "Allow or forbid notifications",
+      }).append($('<a>').attr({
+        'title': 'Allow or forbid notifications',
+        'aria-label': 'Allow or forbid notifications',
+      }).append($('<button>').attr({
+        'id': 'notification-permission-button',
+        'class': 'buttonicon buttonicon-chat',
+        'title': 'Allow or forbid notifications',
+        'aria-label': 'Allow or forbid notifications',
       })));
-      var toolbar = $("#editbar > .menu_left");
+      const toolbar = $('#editbar > .menu_left');
       toolbar.append(allowNotificationsToolbarEntry);
     } else {
       previousEntry.remove();
     }
   },
-  handleNotificationPermission: function(state) {
-    DesktopNotifications.status = state === "granted";
-    DesktopNotifications.allowNotificationsToolbarEntryEnabled(state === "default");
+  handleNotificationPermission(state) {
+    DesktopNotifications.status = state === 'granted';
+    DesktopNotifications.allowNotificationsToolbarEntryEnabled(state === 'default');
   },
-  enable: function() { // enables notifications
-    DesktopNotifications.requestNotificationPermission(function(result) {
-      if (!DesktopNotifications.helpShown && result === "granted") {
+  enable() { // enables notifications
+    DesktopNotifications.requestNotificationPermission((result) => {
+      if (!DesktopNotifications.helpShown && result === 'granted') {
         DesktopNotifications.helpShown = true;
         DesktopNotifications.createNotification(
-          "Notifications enabled",
-          "You can disable notifications in the settings menu",
-          "en"
+            'Notifications enabled',
+            'You can disable notifications in the settings menu',
+            'en',
         );
       }
       DesktopNotifications.handleNotificationPermission(result);
     });
   },
-  disable: function() { // disables notifications
-    DesktopNotifications.handleNotificationPermission("denied");
+  disable() { // disables notifications
+    DesktopNotifications.handleNotificationPermission('denied');
   },
-  getParam: function(sname) {
-    var params = location.search.substr(location.search.indexOf("?")+1);
-    var sval = "";
-    params = params.split("&");
+  getParam(sname) {
+    let params = location.search.substr(location.search.indexOf('?') + 1);
+    let sval = '';
+    params = params.split('&');
     // split param and value into individual pieces
-    for (var i=0; i<params.length; i++) {
-      temp = params[i].split("=");
-      if ( [temp[0]] == sname ) { sval = temp[1]; }
+    for (let i = 0; i < params.length; i++) {
+      temp = params[i].split('=');
+      if ([temp[0]] == sname) { sval = temp[1]; }
     }
     return sval;
   },
-  newMsg: function(authorName, author, text, sticky, timestamp, timeStr) { // Creates a new desktop notification
+  newMsg(authorName, author, text, sticky, timestamp, timeStr) { // Creates a new desktop notification
     if (DesktopNotifications.status) {
       if (author === clientVars.userId) return; // dont show my own!
       DesktopNotifications.createNotification(authorName, text);
     }
-  }
-}
+  },
+};
 
 
-var postAceInit = function(hook, context){
+var postAceInit = function (hook, context) {
   /* initialize properties */
   DesktopNotifications.status = false;
   DesktopNotifications.helpShown = false;
 
   /* init */
-  var $optionsDesktopNotifications = $("#options-desktopNotifications");
-  var urlDesktopNotifications = DesktopNotifications.getParam("DesktopNotifications");
-  if (urlDesktopNotifications === "true") {
-    $optionsDesktopNotifications.attr("checked", "checked");
-  } else if (urlDesktopNotifications === "false") {
-    $optionsDesktopNotifications.attr("checked", false);
+  const $optionsDesktopNotifications = $('#options-desktopNotifications');
+  const urlDesktopNotifications = DesktopNotifications.getParam('DesktopNotifications');
+  if (urlDesktopNotifications === 'true') {
+    $optionsDesktopNotifications.attr('checked', 'checked');
+  } else if (urlDesktopNotifications === 'false') {
+    $optionsDesktopNotifications.attr('checked', false);
   }
-  if($optionsDesktopNotifications.is(":checked")) {
+  if ($optionsDesktopNotifications.is(':checked')) {
     DesktopNotifications.handleNotificationPermission(
-      DesktopNotifications.notificationPermission()
+        DesktopNotifications.notificationPermission(),
     );
   } else {
     DesktopNotifications.disable();
   }
 
   /* on click */
-  $optionsDesktopNotifications.click(function() {
-    if ($optionsDesktopNotifications.is(":checked")) {
+  $optionsDesktopNotifications.click(() => {
+    if ($optionsDesktopNotifications.is(':checked')) {
       DesktopNotifications.enable();
     } else {
       DesktopNotifications.disable();
@@ -133,8 +133,8 @@ var postAceInit = function(hook, context){
 
 exports.postAceInit = postAceInit;
 
-exports.chatNewMessage = function(e, obj, cb){
-  obj.authorName = obj.authorName || "SYSTEM MESSAGE:";
+exports.chatNewMessage = function (e, obj, cb) {
+  obj.authorName = obj.authorName || 'SYSTEM MESSAGE:';
   DesktopNotifications.newMsg(obj.authorName, obj.author, obj.text, obj.sticky, obj.timestamp, obj.timeStr);
   cb([null]);
-}
+};
